ffd7cae42140f782a2f9bd1e2498f5c10a8c2f8c
Dima <dmytro.rets@gamesys.co.uk>
2022-08-30 18:02:19 +0100

    Finish first version of the backend app

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,79 @@
-import express from "express";
-import { encrypt, decrypt } from './crypto';
-import * as fs from 'fs';
+import express from "express"
+import { v4 } from 'uuid'
+import * as fs from 'fs'
+
+import { encrypt, decrypt } from './crypto'
+
+const PORT = 3000
+const VERSION = '/api/v1'
+const pathToStore = process.env.STORE ?? './store'
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
 
-type Contact = {
-    name: string
-    phone: number
-    email: string
-    address: string
+type Contact = { id: string, name: string, phone: number, email: string, address: string }
+
+const save = (password: string, data: string): void => {
+    fs.writeFileSync(pathToStore, JSON.stringify(encrypt(password, data)), 'utf-8')
 }
-const pathToStore = './store'
-const myContact: Contact = { name: 'Dima', phone: 999, email: 'a@a.com', address: 'London' }
 
-app.get("/encrypt/:password", function (req, res) {
-    const encrypted = encrypt(req.params.password, JSON.stringify(myContact))
-    fs.writeFileSync(pathToStore, JSON.stringify(encrypted), 'utf-8')
-    res.send("Encrypted");
+const getContacts = (password: string): Contact[] => {
+    if (!fs.existsSync(pathToStore)) return [];
+    const encrypted = fs.readFileSync(pathToStore, 'utf-8');
+    return JSON.parse(decrypt(password, JSON.parse(encrypted)));
+}
+
+app.post(`${VERSION}/contact`, function (req, res) {
+    if (typeof req.query.password !== 'string') return res.sendStatus(400)
+    let contacts = []
+    try {
+        contacts = getContacts(req.query.password as string)
+    } catch (e) {
+        return res.sendStatus(401)
+    }
+    if (Object.keys(req.body).length !== 0) {
+        const newContact = { ...req.body, id: v4() } as Contact
+        contacts.push(newContact)
+    }
+    save(req.query.password as string, JSON.stringify(contacts))
+    res.sendStatus(200)
 });
 
-app.get("/decrypt/:password", function (req, res) {
-    const encrypted = fs.readFileSync(pathToStore, 'utf-8')
+app.put(`${VERSION}/contact/:id`, function (req, res) {
+    if (typeof req.query.password !== 'string') return res.sendStatus(400)
+    let contacts = []
     try {
-        const decrypted = decrypt(req.params.password, JSON.parse(encrypted))
-        res.send("Decrypted: " + decrypted);
+        contacts = getContacts(req.query.password as string)
     } catch (e) {
-        res.send("Wrong password")
+        return res.sendStatus(401)
+    }
+    const index = contacts.findIndex(contact => contact.id === req.params.id)
+    if (index === -1) res.sendStatus(403)
+    contacts[index] = { ...contacts[index], ...req.body }
+    save(req.query.password as string, JSON.stringify(contacts))
+    res.sendStatus(200)
+});
+
+app.get(`${VERSION}/contacts`, function (req, res) {
+    if (typeof req.query.password !== 'string') return res.sendStatus(400)
+    let contacts = []
+    try {
+        contacts = getContacts(req.query.password as string)
+        res.send(contacts)
+    } catch (e) {
+        return res.sendStatus(401)
     }
 });
 
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+app.head(`${VERSION}/store`, function (req, res) {
+    if (fs.existsSync(pathToStore)) {
+        return res.sendStatus(200)
+    } else {
+        return res.sendStatus(404)
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`)
 });
