7904ca17d92fc495c1f4045d252fe3a32479a671
Dima <dmytro.rets@gamesys.co.uk>
2022-08-30 14:20:55 +0100

    Refactor crypto

--- src/crypto.ts
+++ src/crypto.ts
@@ -1,15 +1,15 @@
 import * as crypto from 'crypto';
 
-const algorithm = 'aes-128-cbc';
-const keySize = 16;
-const salt = crypto.randomBytes(16);
-const iv = crypto.randomBytes(16);
+const ALGO = 'aes-128-cbc';
+const KEY_SIZE = 16;
 
 type Encrypted = { iv: string, salt: string, encrypted: string }
 
 export const encrypt = (password: string, text: string): Encrypted => {
-    const key = crypto.scryptSync(password, salt, keySize);
-    const cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+    const salt = crypto.randomBytes(16);
+    const iv = crypto.randomBytes(16);
+    const key = crypto.scryptSync(password, salt, KEY_SIZE);
+    const cipher = crypto.createCipheriv(ALGO, Buffer.from(key), iv);
     return {
         iv: iv.toString('hex'),
         salt: salt.toString('hex'),
@@ -20,8 +20,8 @@ export const encrypt = (password: string, text: string): Encrypted => {
 export const decrypt = (password: string, text: Encrypted): string => {
     const iv = Buffer.from(text.iv, 'hex');
     const salt = Buffer.from(text.salt, 'hex');
-    const key = crypto.scryptSync(password, salt, keySize);
+    const key = crypto.scryptSync(password, salt, KEY_SIZE);
     const encryptedText = Buffer.from(text.encrypted, 'hex');
-    const decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+    const decipher = crypto.createDecipheriv(ALGO, Buffer.from(key), iv);
     return Buffer.concat([decipher.update(encryptedText), decipher.final()]).toString();
 }
