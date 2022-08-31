06acf85a480328612f4e4208d5923d1eda343149
Dima <dmytro.rets@gamesys.co.uk>
2022-08-31 22:10:44 +0100

    Update readme

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 To run the application please follow `README.md` instructions in `server` and `client` folders.  
 
-Tested on node v16.17.0/macOS/Chrome
+Essentially, the application has server-client architecture. Server is written using `express`, saving encrypted data into a file.
+Client is written using `Svelte`, making REST API calls to the server.
+
+Tested on node v16.17.0/macOS/Chrome.
--- client/src/routes/api.ts
+++ client/src/routes/api.ts
@@ -1,12 +1,4 @@
-/*
-	This module is used by the /todos endpoint to
-	make calls to api.svelte.dev, which stores todos
-	for each user.
-
-	(The data on the todo app will expire periodically; no
-	guarantees are made. Don't use it to organise your life.)
-*/
-export const SERVER_URL = 'http://localhost:3001/api/v1'
+export const SERVER_URL = `${import.meta.env.VITE_SERVER}/api/v1`
 
 export function api(method: string, resource: string, data?: Record<string, unknown>) {
 	return fetch(`${SERVER_URL}/${resource}`, {
